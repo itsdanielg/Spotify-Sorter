8302511e43cd1fefdaf0b3400c9182d935d90196
itsdanielg <danielg_97@yahoo.com>
2023-09-25 13:14:32 -0400

    Added spotify types

---
+++ src/types/spotify.ts
@@ -0,0 +1,101 @@
+type SpotifyPlaylist = {
+  id: string;
+  name: string;
+  collaborative: boolean;
+  description: string | null;
+  images: SpotifyImage[];
+  owner: SpotifyOwner;
+  public: boolean;
+  tracks: SpotifyPlaylistTracks;
+  type: "playlist";
+};
+
+type SpotifyImage = {
+  height: number | null;
+  url: string;
+  width: number | null;
+};
+
+type SpotifyOwner = {
+  id: string;
+  display_name: string | null;
+  type: "user";
+};
+
+type SpotifyPlaylistTracks = {
+  href: string;
+  items: SpotifyPlaylistTrack[];
+  limit: number;
+  next: string | null;
+  offset: number;
+  previous: string | null;
+  total: number;
+};
+
+type SpotifyPlaylistTrack = {
+  added_at: string;
+  added_by: SpotifyOwner;
+  is_local: boolean;
+  track: SpotifyTrack;
+};
+
+type SpotifyTrack = {
+  id: string;
+  name: string;
+  album: SpotifyAlbum;
+  artists: SpotifyArtist[];
+  duration_ms: number;
+  explicit: boolean;
+  href: string;
+  is_local: boolean;
+  is_playable: boolean;
+  popularity: number;
+  preview_url: string | null;
+  restrictions: SpotifyRestriction;
+  track_number: number;
+  type: "track";
+};
+
+type SpotifyAlbum = {
+  id: string;
+  name: string;
+  album_type: "album" | "single" | "compilation";
+  artists: SpotifyArtist[];
+  href: string;
+  images: SpotifyImage[];
+  release_date: string;
+  release_date_precision: "year" | "month" | "day";
+  restrictions: SpotifyRestriction;
+  total_tracks: number;
+  type: "album";
+};
+
+type SpotifySimplifiedArtist = {
+  id: string;
+  name: string;
+  href: string;
+  type: "artist";
+};
+
+type SpotifyArtist = SpotifySimplifiedArtist & {
+  genres: string[];
+  images: SpotifyImage[];
+  popularity: number;
+};
+
+type SpotifyRestriction = {
+  reason: "market" | "product" | "explicit";
+};
+
+export type {
+  SpotifyPlaylist,
+  SpotifyImage,
+  SpotifyOwner,
+  SpotifyPlaylistTracks,
+  SpotifyPlaylistTrack,
+  SpotifyTrack,
+  SpotifyAlbum,
+  SpotifySimplifiedArtist,
+  SpotifyArtist,
+  SpotifyRestriction
+};
